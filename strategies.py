092199bd9dccd02d16b024037428ff8760783309
sporeball <sporeballdev@gmail.com>
2023-04-11 23:16:02 -0700

    simple lookahead when evaluating checks

--- strategies.py
+++ strategies.py
@@ -11,19 +11,45 @@ class ExampleEngine(MinimalEngine):
 class Lucy(ExampleEngine):
   def evaluate(self, board: chess.Board, move) -> float:
     ev = 0.0
-    isCapture = board.is_capture(move)
-    isCheck = board.gives_check(move)
-    # board.push(move)
-    # board.pop()
-    if isCapture:
+    moveIsCapture = board.is_capture(move)
+    moveIsCheck = board.gives_check(move)
+    # captures are good
+    if moveIsCapture:
       ev += 0.1
-    if isCheck:
+    # checks are better
+    if moveIsCheck:
       ev += 0.5
+
+    # push the move
+    board.push(move)
+
+    # get list of the opponent's responses
+    legalResponses = list(board.legal_moves)
+    # if the move Lucy played is a check,
+    # for each response,
+    if moveIsCheck:
+      for response in legalResponses:
+        # if the response is a capture by something other than the king,
+        # or the response's source square has a pawn on it,
+        # it might not be a good idea to play that check
+        if (
+          board.is_capture(response) and
+          board.piece_type_at(response.from_square) != chess.KING
+        ):
+          ev -= 0.4
+        if board.piece_type_at(response.from_square) == chess.PAWN:
+          ev -= 0.4
+
+    # pop the move
+    board.pop()
+
     return ev
 
   def search(self, board: chess.Board, *args: Any) -> PlayResult:
     # list of legal moves
     legalMoves = list(board.legal_moves)
+    numLegalMoves = len(legalMoves)
+    print(f'number of legal moves: {numLegalMoves}')
     # list of options for best move
     bestMoves = []
     # the highest evaluation of any move so far
